4ad5755cd13e7c7d3cd3c9afefba9b2612f16461
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-02-09 23:46:22 -0600

    registered User, Game, and Pick models in picks/admin.py

--- picks/admin.py
+++ picks/admin.py
@@ -1,3 +1,16 @@
+# picks/admin.py
 from django.contrib import admin
+from .models import User, Game, Pick
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'phone_number')
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('team', 'week', 'opponent')
+
+@admin.register(Pick)
+class PickAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'pick')
+    list_filter = ('pick', 'game__week', 'user')
